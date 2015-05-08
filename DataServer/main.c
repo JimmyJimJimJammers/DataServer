7a2699295fbda33ceaabf582984bd04f01ef50d6
JimmyJimJimJammers <mccarthyj10@aol.com>
2015-05-08 18:12:18 -0400

    first commit

---
+++ DataServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataServer
+//
+//  Created by James McCarthy on 5/8/15.
+//  Copyright (c) 2015 JimMcCarthy3D. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
